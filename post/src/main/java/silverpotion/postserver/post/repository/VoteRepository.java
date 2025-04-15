20d4946e6558834de13572ed9ed4ee757a07fb47
leejaeseok-98 <wotjrdl98@naver.com>
2025-04-15 14:55:18 +0900

    fix/post

---
+++ post/src/main/java/silverpotion/postserver/post/repository/VoteRepository.java
@@ -0,0 +1,24 @@
+package silverpotion.postserver.post.repository;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RequestBody;
+import silverpotion.postserver.common.domain.DelYN;
+import silverpotion.postserver.post.domain.Post;
+import silverpotion.postserver.post.domain.PostCategory;
+import silverpotion.postserver.post.domain.PostStatus;
+import silverpotion.postserver.post.domain.Vote;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface VoteRepository extends JpaRepository<Vote,Long> {
+    Optional<Vote> findVoteByVoteId(Long voteId);
+
+    //    게시물 페이징 조회
+    Page<Vote> findByWriterIdInAndPostCategoryAndDelYnAndPostStatus(List<Long> writerIds, PostCategory postCategory, DelYN delYN, PostStatus postStatus, Pageable pageable);
+
+}

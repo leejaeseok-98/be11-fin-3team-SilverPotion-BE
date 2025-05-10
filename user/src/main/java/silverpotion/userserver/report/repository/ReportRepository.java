43e313bec61ecb81ea311c35157278280011bfd0
leejaeseok-98 <wotjrdl98@naver.com>
2025-05-10 23:55:12 +0900

    report

--- user/src/main/java/silverpotion/userserver/report/repository/ReportRepository.java
+++ user/src/main/java/silverpotion/userserver/report/repository/ReportRepository.java
@@ -5,14 +5,16 @@ import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import silverpotion.userserver.report.domain.Report;
 import silverpotion.userserver.report.domain.ReportBigCategory;
+import silverpotion.userserver.report.domain.ReportSmallCategory;
 import silverpotion.userserver.user.domain.User;
 
 @Repository
 public interface ReportRepository extends JpaRepository<Report,Long>, JpaSpecificationExecutor<Report> {
-    boolean existsByReporterAndReportedIdAndReportBigCategoryAndReferenceId(
+    boolean existsByReporterAndReportedIdAndReportBigCategoryAndReportSmallCategoryAndReferenceId(
             User reporter,
             User reportedId,
             ReportBigCategory reportBigCategory,
+            ReportSmallCategory reportSmallCategory,
             Long referenceId
     );
 }

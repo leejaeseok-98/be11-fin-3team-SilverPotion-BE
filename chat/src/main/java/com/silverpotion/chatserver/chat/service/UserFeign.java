71220a6ad2c2ffa91e3ad9011f240ebff4391ace
issac-cosmos <rud____@naver.com>
2025-05-11 15:42:31 +0900

    feign 오류 잡기

--- chat/src/main/java/com/silverpotion/chatserver/chat/service/UserFeign.java
+++ chat/src/main/java/com/silverpotion/chatserver/chat/service/UserFeign.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "user-service", configuration = FeignConfig.class)
+@FeignClient(name = "user-service", path = "silverpotion/user", configuration = FeignConfig.class)
 public interface UserFeign {
 
     @GetMapping("silverpotion/user/userId")

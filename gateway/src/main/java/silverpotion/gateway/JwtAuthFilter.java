c9fd753e7f8002aae9adc7d049eccc1e0f9b423c
leejaeseok-98 <wotjrdl98@naver.com>
2025-05-13 10:22:41 +0900

    coolSms

--- gateway/src/main/java/silverpotion/gateway/JwtAuthFilter.java
+++ gateway/src/main/java/silverpotion/gateway/JwtAuthFilter.java
@@ -42,7 +42,8 @@ public class JwtAuthFilter implements GlobalFilter {
             "/chat-service/**/xhr*",                // fallback transport 경로들
             "/chat-service/**/eventsource",         // fallback transport
             "/chat-service/**/htmlfile" ,            // 일부 브라우저 fallback
-            "/sse/**"
+            "/sse/**",
+            "/silverpotion/sms/**"
 
     );
 
--- user/src/main/java/silverpotion/userserver/common/dto/AutoCodeDto.java
+++ user/src/main/java/silverpotion/userserver/common/dto/AutoCodeDto.java
@@ -9,5 +9,6 @@ import lombok.NoArgsConstructor;
 @Data
 public class AutoCodeDto {
     private String phoneNumber;
-    private String autoCode;
+    private String autoCode; // 인증번호 생성용
+    private String inputCode; // 클라이언트가 입력한 인증번호 검증용
 }
